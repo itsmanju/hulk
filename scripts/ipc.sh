297f80c81416088cd0061a0cf5720f42f3fb85fb
Abhijit Das <abhijitcse@yahoo.in>
2025-05-14 14:27:45 +0000

    Initial commit

---
+++ scripts/ipc.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+cd /home/manju/work/hulk
+
+for num in `ls m5out/`
+do
+	sum=0.0
+	for val in `grep "ipc_total" m5out/$num/64-core/stats.txt | head -n 64 | tr -s ' ' | cut -d ' ' -f 2`
+	do
+		sum=`echo $sum + $val | bc`
+	done
+	echo "$num,$sum" >> ipc_total_64.csv
+done
